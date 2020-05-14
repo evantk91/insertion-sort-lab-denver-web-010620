ca1d47a5f6043155c10e41189190f63214b5a6fd
etgreer91 <etgreer91@gmail.com>
2020-05-14 08:45:11 -1000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
-
+    let minIdx
+    let min = array[0]
+    for(let i = 0; i < array.length; i++) {
+        let element = array[i]
+        if(element < min) {
+            min = element;
+            minIdx = i 
+        }
+    }
+    array.splice(minIdx, 1)
+    return min;
 }
 
 function insertionSort(array){
-
+    let newMin; 
+    let sortedArr = [];
+    while(array.length > 0) {
+        newMin = findMinAndRemove(array)
+        sortedArr.push(newMin)
+    }
+    return sortedArr
 }
